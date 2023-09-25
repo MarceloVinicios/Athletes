514f8a6e082d321465ad8c91a0deec0cc6ad1ff2
Luiz Felipe <luizfelipealvesdesouza706@gmail.com>
2023-09-25 16:27:11 -0300

    <AF-13> Componente SideBar finalizado

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,9 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import LoginButton from "../../auth/LoginButton";
+import SingUp from "../../auth/SingUp";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { useAuth0 } from "@auth0/auth0-react";
+import { ButtonLoading, ContainerMenu, ContainerUser, Header, ImageProfile, Search } from "./StyledHeader";
+import { LinkNavigation, LinkNavigationMenu, ListMenuNavigation, MenuActive, NavbarLinks, NavbarLogo, Navigation } from "./StyledNavBar";
 
-const Header = () => {
+const Navbar = () => {
+  const { user, isAuthenticated, isLoading, logout } = useAuth0();
+  const [MenuActivite, setMenuActive] = useState(false);
+
   return (
-   <div>oi</div>
+    <Header>
+      <NavbarLogo href="/feed">
+        <img src="src\assets\logos\Logo.svg" alt="logo" />
+      </NavbarLogo>
+
+      <Navigation aria-label="Login">
+        <NavbarLinks>
+          <LinkNavigation>
+            <LoginButton />
+          </LinkNavigation>
+          <LinkNavigation>
+            <SingUp />
+          </LinkNavigation>
+        </NavbarLinks>
+      </Navigation>
+
+      {isAuthenticated && (
+        <Search type="text" name="search" placeholder="Buscar" />
+      )}
+      {isAuthenticated && (
+        <ContainerUser>
+          <ButtonLoading href="#">Carregar</ButtonLoading>
+          <ContainerMenu>
+            <ImageProfile src={user.picture} alt={user.name} />
+            <MenuActive>
+              <MdKeyboardArrowDown
+                size={"20px"}
+                onClick={() => setMenuActive(!MenuActivite)}
+                style={{ cursor: "pointer" }}
+              />
+              <nav aria-label="Primaria">
+                <ListMenuNavigation
+                  style={{ display: MenuActivite ? "block" : "none" }}
+                >
+                  <LinkNavigationMenu>Profile</LinkNavigationMenu>
+                  <LinkNavigationMenu>
+                    <a onClick={() => logout()}>Logout</a>
+                  </LinkNavigationMenu>
+                </ListMenuNavigation>
+              </nav>
+            </MenuActive>
+          </ContainerMenu>
+        </ContainerUser>
+      )}
+    </Header>
   );
 };
 
-export default Header;
+export default Navbar;
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,23 +1,23 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Apresentation/Home";
 import Header from "../components/common/Header/Header";
-import NotFound from "../routes/NotFound";
-import Publication from "../components/Publication/Publication";
 import Feed from "./Feed";
+import Public from "./../components/Publication/Publication";
+import NotFound from "../routes/NotFound";
 
 function App() {
   return (
-    <BrowserRouter>
-      <ChakraProvider>
+    <ChakraProvider>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="feed" element={      <Feed />} />
+          <Route path="feed" element={<Feed />} />
           <Route path="*" element={<NotFound />} />
-          <Route path="/Publi" element={< Publication />} />
+          <Route path="public" element={<Public />} />
         </Routes>
-      </ChakraProvider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </ChakraProvider>
   );
 }
 
