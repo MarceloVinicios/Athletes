a710bb9ce8630f84878bd96abbef4b60f2fbcf44
Marcelo Vinicios <marcelo1.dev@gmail.com>
2023-10-09 23:57:58 -0300

    Contador de likes adicionado

--- src/components/ui/Publication/Interation/Like.jsx
+++ src/components/ui/Publication/Interation/Like.jsx
@@ -1,25 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { MdStarBorder, MdStar } from "react-icons/md";
 import { Button } from "@chakra-ui/react";
 
 function Like() {
-  const [liked, setLiked] = useState(false);
+ const [liked, setLiked] = useState(false);
+ const [count, setCount] = useState(0);
 
-  return (
+ const handleClick = () => {
+    setLiked(!liked);
+    setCount(liked ? (count > 0 ? count - 1 : 0) : count + 1);
+ };
+
+ return (
     <div>
       <Button
-        variant="ghost" justifySelf={"start"}
-        onClick={() => setLiked(!liked)}
+        variant="ghost"
+        justifySelf={"start"}
+        onClick={handleClick}
       >
         {liked ? (
-         <MdStar fill="#ebc556" fontSize={"24px"} />
+          <MdStar fill="#ebc556" fontSize={"24px"} />
         ) : (
           <MdStarBorder fontSize={"24px"} />
         )}
-        Gostei
+        {count} Gostei
       </Button>
     </div>
-  );
+ );
 }
 
 export default Like;
