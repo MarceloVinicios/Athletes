c27eb125261aac01e82cd8eddf34b1483ab58e13
Luiz Felipe <luizfelipealvesdesouza706@gmail.com>
2023-11-29 21:45:34 -0300

    foi relacionado a publicação a uma categoria na hora de publicar

--- src/components/ui/NewPublication/FormUpload/FormUpload.jsx
+++ src/components/ui/NewPublication/FormUpload/FormUpload.jsx
@@ -20,19 +20,18 @@ import { useAuth0 } from "@auth0/auth0-react";
 import useFetch from "../../../../hooks/useFetch";
 import LoadingContainer from "../../../helper/LoadingContainer";
 import Successfully from "../../../helper/Successfully";
-import FeedContext  from "../../../../pages/Feed/FeedContext"
+import FeedContext from "../../../../pages/Feed/FeedContext";
 import { GetAllCategory } from "../../../../api/CategoryApi";
 
 const FormUpload = () => {
-  const [successfully, setSucessfully] = useState(false);
+  const [successfully, setSuccessfully] = useState(false);
   const [file, setFile] = useState(null);
-  const [description, setDescription] = useState('')
+  const [description, setDescription] = useState("");
   const [preview, setPreview] = useState(null);
-  const [categoryResponse, SetCategoryResponse] = useState('');
+  const [categoryResponse, setCategoryResponse] = useState(1);
   const [categoriesApi, setCategoriesApi] = useState([]);
   const { loading, error, request } = useFetch();
   const { getAccessTokenSilently } = useAuth0();
-  const dataFeedContext = useContext(FeedContext)
 
   const handleFileChange = ({ target }) => {
     const selectedFile = target.files[0];
@@ -55,24 +54,27 @@ const FormUpload = () => {
       const { url, options } = GetAllCategory(token);
       const { response, json } = await request(url, options);
       if (response.status === 200) {
-        setCategoriesApi(json.categoriesData)
+        setCategoriesApi(json.categoriesData);
       }
     }
     getAllCategoryOfApi();
   }, [getAccessTokenSilently, request]);
 
   const handleUpload = async () => {
-    if (file && description) {
+    console.log(categoryResponse)
+    if (file && description && categoryResponse) {
       const formData = new FormData();
       formData.append("file", file);
       formData.append("description", description);
+      formData.append("category", categoryResponse);
+      console.log(formData)
+      console.log(Number(categoryResponse));
 
       const token = await getAccessTokenSilently();
       const { url, options } = PostPublication(formData, token);
       const { response } = await request(url, options);
       if (response.status === 201) {
-        setSucessfully(true);
-        dataFeedContext.reloadPublications();
+        setSuccessfully(true);
       }
     }
   };
@@ -95,62 +97,64 @@ const FormUpload = () => {
 
   return (
     <FormContainer>
-    <FormHeader>Formulário</FormHeader>
-    <FormContent>
-      <FileContainer>
-        <form>
-          <div>
-            <FileInput
-              type="file"
-              accept="image/*,video/*"
-              id="fileInput"
-              onChange={handleFileChange}
-            />
-            <FileLabel htmlFor="fileInput">Selecionar Arquivo</FileLabel>
-          </div>
-        </form>
-        <PreviewContainer>
-          {file && preview && (
-            <>
-              {file.type.includes("image") ? (
-                <PreviewImage src={preview} alt="Preview" />
-              ) : (
-                <PreviewVideo controls>
-                  <source src={preview} type={file.type} />
-                </PreviewVideo>
-              )}
-            </>
-          )}
-        </PreviewContainer>
-      </FileContainer>
-      <DescriptionContainer>
+      <FormHeader>Formulário</FormHeader>
+      <FormContent>
+        <FileContainer>
+          <form>
+            <div>
+              <FileInput
+                type="file"
+                accept="image/*,video/*"
+                id="fileInput"
+                onChange={handleFileChange}
+              />
+              <FileLabel htmlFor="fileInput">Selecionar Arquivo</FileLabel>
+            </div>
+          </form>
+          <PreviewContainer>
+            {file && preview && (
+              <>
+                {file.type.includes("image") ? (
+                  <PreviewImage src={preview} alt="Preview" />
+                ) : (
+                  <PreviewVideo controls>
+                    <source src={preview} type={file.type} />
+                  </PreviewVideo>
+                )}
+              </>
+            )}
+          </PreviewContainer>
+        </FileContainer>
+        <DescriptionContainer>
           <LabelDescription htmlFor="category">Categoria:</LabelDescription>
           <SelectSport
             id="category"
             name="category"
             value={categoryResponse}
-            onChange={(e) => SetCategoryResponse(e.target.value)}
+            onChange={(e) => setCategoryResponse(e.target.value)}
           >
-            {categoriesApi.map(category => (
-              <option value={category.id} key={category.id}>{category.category}</option>
+            {categoriesApi.map((category) => (
+              <option value={category.id} key={category.id}>
+                {category.category}
+              </option>
             ))}
           </SelectSport>
-        <LabelDescription htmlFor="description">Descrição:</LabelDescription>
-        <DescriptionArea
-          id='description'
-          name="description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          rows="4"
-          cols="50"
-        ></DescriptionArea>
-        {file && (
-          <CustomButton onClick={handleUpload}>Enviar Arquivo</CustomButton>
-        )}
-      </DescriptionContainer>
-    </FormContent>
-  </FormContainer>
-  )
-}
+          <LabelDescription htmlFor="description">Descrição:</LabelDescription>
+          <DescriptionArea
+            id="description"
+            name="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            rows="4"
+            cols="50"
+          ></DescriptionArea>
+          {file && (
+            <CustomButton onClick={handleUpload}>Enviar Arquivo</CustomButton>
+          )}
+        </DescriptionContainer>
+      </FormContent>
+    </FormContainer>
+  );
+};
 
 export default FormUpload;
